85947b0f01bd04c13fc52308657b96aa8d824152
maheshvane07 <131536495+maheshvane07@users.noreply.github.com>
2023-04-25 21:17:14 +0530

    Add files via upload

---
+++ Hotel.java
@@ -0,0 +1,10 @@
+interface Hotel 
+{
+	String hotelName="Royal Hotel";
+	double total=0.0;
+	
+	public double displayBill(String name,int table_no);
+	public void orderFood();		
+
+	public double getTotal();
+}
